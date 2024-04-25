c8355ddf868d9a52766bac432d7923d3274cd681
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-25 10:16:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementById('fname').addEventListener('blur', function() {
+    // Get the current value of the input field
+    let currentValue = this.value;
+
+    // Convert the value to uppercase
+    let newValue = currentValue.toUpperCase();
+
+    // Set the new value of the input field
+    this.value = newValue;
+});
